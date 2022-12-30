c0139e9f565321f4cbee8d05b5da82402fca06bd
chris abdo <chris.j.abdo@gmail.com>
2022-12-30 17:52:21 -0500

    ADDED: update to authentication flow with thirdweb sdk

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import { useState } from 'react';
 import Marquee from 'react-fast-marquee';
-import { ConnectWallet } from '@thirdweb-dev/react';
+import {
+  ConnectWallet,
+  useNetworkMismatch,
+  useAddress,
+  useNetwork,
+} from '@thirdweb-dev/react';
 
 const Navbar = ({ account, Web3Handler, disconnectWallet }) => {
   // function to hide id="alert" on click
   const hideAlert = () => {
     document.getElementById('alert').style.display = 'none';
   };
+
+  const address = useAddress();
+  const isOnWrongNetwork = useNetworkMismatch();
+  const [, switchNetwork] = useNetwork();
+  const [networkManager, setNetworkManager] = useState();
+
+  const CHAIN_ID = 80001;
+
+  if (isOnWrongNetwork) {
+    return (
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-white text-center">
+          <h1 className="text-2xl font-bold text-red-500">Wrong Network</h1>
+          <p className="mt-2">
+            Please switch to the{' '}
+            <span className="text-purple-500">Mumbai Testnet</span> to use
+            Lofichain.
+          </p>
+        </div>
+        <button
+          onClick={() => switchNetwork(CHAIN_ID)}
+          className="mt-4 px-4 py-2 text-white bg-[#1a1a1a] rounded-md hover:bg-[#2a2a2a]"
+        >
+          Switch Network
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div className="">
@@ -111,74 +144,7 @@ const Navbar = ({ account, Web3Handler, disconnectWallet }) => {
           </ul>
         </div>
         <div className="navbar-end text-white">
-          {account ? (
-            <div className="dropdown dropdown-end ">
-              <label
-                tabIndex={0}
-                className="cursor-pointer relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group "
-              >
-                <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
-                {/* <!-- Top glass gradient --> */}
-                <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
-                {/* <!-- Bottom gradient --> */}
-                <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
-                {/* <!-- Left gradient --> */}
-                <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
-                {/* <!-- Right gradient --> */}
-                <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
-                <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
-                <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
-                <span className="relative">
-                  {account.slice(0, 5)}...{account.slice(-4)}
-                </span>
-              </label>
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu p-2 shadow bg-[#1a1a1a]  text-white rounded-box w-52 mt-3 text-center items-center"
-              >
-                <li>
-                  <a className="hover:bg-[#2a2a2a]">
-                    Profile{' '}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                      />
-                    </svg>
-                  </a>
-                </li>
-                <li>
-                  <a onClick={disconnectWallet} className="hover:bg-[#2a2a2a]">
-                    Disconnect{' '}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-                      />
-                    </svg>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          ) : (
-            <ConnectWallet />
-          )}
+          <ConnectWallet />
         </div>
       </div>
     </div>
@@ -186,20 +152,3 @@ const Navbar = ({ account, Web3Handler, disconnectWallet }) => {
 };
 
 export default Navbar;
-
-{
-  /* <button
-              onClick={Web3Handler}
-              className="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group"
-            >
-              <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
-     
-              <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
-              <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
-              <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
-              <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
-              <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
-              <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
-              <span className="relative">Connect Wallet</span>
-            </button> */
-}
--- pages/index.jsx
+++ pages/index.jsx
@@ -87,23 +87,43 @@ const Home = () => {
                 The Web3 radio station that rewards creators and listeners
               </Balancer>
             </p>
-            <button
-              onClick={Web3Rerouter}
-              className="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group"
-            >
-              <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
-              {/* <!-- Top glass gradient --> */}
-              <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
-              {/* <!-- Bottom gradient --> */}
-              <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
-              {/* <!-- Left gradient --> */}
-              <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
-              {/* <!-- Right gradient --> */}
-              <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
-              <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
-              <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
-              <span className="relative">Get Started</span>
-            </button>
+
+            {/* only display a button if the users wallet is connected */}
+            {web3Provider ? (
+              <button
+                onClick={() => (window.location.href = '/radio')}
+                className="mt-6 relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group"
+              >
+                <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
+                {/* <!-- Top glass gradient --> */}
+                <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
+                {/* <!-- Bottom gradient --> */}
+                <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
+                {/* <!-- Left gradient --> */}
+                <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
+                {/* <!-- Right gradient --> */}
+                <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
+                {/* <!-- Text --> */}
+                <span className="relative z-10">Enter Lofichain</span>
+              </button>
+            ) : (
+              <button
+                onClick={Web3Rerouter}
+                className="mt-6 relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group"
+              >
+                <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
+                {/* <!-- Top glass gradient --> */}
+                <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
+                {/* <!-- Bottom gradient --> */}
+                <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
+                {/* <!-- Left gradient --> */}
+                <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
+                {/* <!-- Right gradient --> */}
+                <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
+                {/* <!-- Text --> */}
+                <span className="relative z-10">Connect Wallet</span>
+              </button>
+            )}
           </div>
         </div>
       </div>
